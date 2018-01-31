e756bb75fb0cae91b9657d46434d72036caf28b1
Dmitry Ryumkin <riumkin@gmail.com>
2018-01-31 11:53:34 +0530

    getting the cards to stack

--- src/Deck.js
+++ src/Deck.js
@@ -86,22 +86,34 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             { ...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
           </Animated.View>
         )
       }
-      return this.props.renderCard(item)
-    })
+      return (
+        <View key={item.id} style={styles.cardStyle}>
+          {this.props.renderCard(item)}
+        </View>
+      )
+    }).reverse()
   }
+
   render() {
     return (
       <Animated.View>
         {this.renderCards()}
       </Animated.View>
     )
+  }
+}
+
+const styles = { 
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
   }
 }
 
